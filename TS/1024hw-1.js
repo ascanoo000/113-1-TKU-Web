391e90be256371c5f12527f1a9835513e400502a
ascanoo000 <danny06250820@gmail.com>
2024-10-24 12:02:45 +0800

    TS

---
+++ TS/1024hw-1.js
@@ -0,0 +1,6 @@
+var age = 25;
+console.log("Age: ".concat(age));
+var name = "Alice";
+console.log("\u66F4\u6539\u524D: ".concat(name));
+name = "Bob";
+console.log("\u66F4\u6539\u5F8C: ".concat(name));
---
+++ TS/1024hw-1.ts
@@ -0,0 +1,8 @@
+const age: number = 25;
+console.log(`Age: ${age}`);
+
+let name: string = "Alice";
+console.log(`更改前: ${name}`);
+
+name = "Bob";
+console.log(`更改後: ${name}`);
